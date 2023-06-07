c1f5570af71511e8767801da36c943bfd8d756d4
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2023-06-07 19:49:01 -0300

    pegando com erro

--- App.js
+++ App.js
@@ -2,12 +2,55 @@ import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
-  SectionList,
   Text,
   View,
-  StatusBar,
   Image,
+  StyleSheet,
 } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
+
+const MovieScreen = ({ filmes }) => (
+  <FlatList
+    data={filmes}
+    renderItem={({ item }) => (
+      <View style={styles.movieContainer}>
+        <Image
+          style={styles.moviePoster}
+          source={{
+            uri: item.poster,
+          }}
+        />
+        <View style={styles.movieDetails}>
+          <Text style={styles.movieTitle}>{item.nome}</Text>
+          <FlatList
+            data={item.sessoes}
+            renderItem={({ item }) => (
+              <View>
+                <Text>
+                  {item.tecnologia} {item.linguagem}
+                </Text>
+                <View>
+                  <FlatList
+                    data={item.horarios}
+                    horizontal
+                    renderItem={({ item }) => (
+                      <View style={styles.showtimeContainer}>
+                        <Text style={styles.showtimeText}>{item}</Text>
+                      </View>
+                    )}
+                  />
+                </View>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    )}
+  />
+);
 
 const App = () => {
   const [isLoading, setLoading] = useState(true);
@@ -32,60 +75,55 @@ const App = () => {
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 24 }}>
+    <View style={styles.container}>
       {isLoading ? (
         <ActivityIndicator />
       ) : (
-        <View>
-          <FlatList
-            data={dados}
-            renderItem={({ item }) => (
-              <View>
-                <Text>{item.data}</Text>
-                <FlatList
-                  data={item.filmes}
-                  renderItem={({ item }) => (
-                    <View style={{ flexDirection: "row", padding: 10 }}>
-                      <Image
-                        style={{ width: 100, height: 150 }}
-                        source={{
-                          uri: item.poster,
-                        }}
-                      />
-                      <View style={{ marginLeft: 5 }}>
-                        <Text>{item.nome}</Text>
-                        <FlatList
-                          data={item.sessoes}
-                          renderItem={({ item }) => (
-                            <View>
-                              <Text>
-                                {item.tecnologia} {item.linguagem}
-                              </Text>
-                              <View>
-                                <FlatList
-                                  data={item.horarios}
-                                  horizontal
-                                  renderItem={({ item }) => (
-                                    <View>
-                                      <Text> {item} </Text>
-                                    </View>
-                                  )}
-                                />
-                              </View>
-                            </View>
-                          )}
-                        />
-                      </View>
-                    </View>
-                  )}
-                />
-              </View>
-            )}
-          />
-        </View>
+        <NavigationContainer>
+          <Tab.Navigator>
+            {dados.map((item, index) => (
+              <Tab.Screen
+                key={index}
+                name={item.data ? item.data.toString() : `Tab${index}`}
+                component={() => <MovieScreen filmes={item.filmes} />}
+              />
+            ))}
+          </Tab.Navigator>
+        </NavigationContainer>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  movieContainer: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  moviePoster: {
+    width: 100,
+    height: 150,
+  },
+  movieDetails: {
+    marginLeft: 5,
+  },
+  movieTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  showtimeContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 4,
+    marginRight: 8,
+  },
+  showtimeText: {
+    fontSize: 12,
+  },
+});
 
 export default App;
